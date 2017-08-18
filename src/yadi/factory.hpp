a1b4fda45714b93867516ce0af0906d8d2360e83
Ed Clark <ebclark2@gmail.com>
2017-08-17 21:31:25 -0400

    Formatted code

--- src/yadi/factory.hpp
+++ src/yadi/factory.hpp
@@ -6,70 +6,57 @@
 #include <memory>
 #include <string>
 
-namespace yadi 
-{
+namespace yadi {
 
-    template <typename BaseType>
-    struct factory_traits
-    {
-        using ptr_type = std::shared_ptr<BaseType>;
-    };
-
-    template <typename BaseType>
-    using ptr_type_t = typename factory_traits<BaseType>::ptr_type;
-
-template <typename BaseType>
-class factory
-{
-public:
-    using base_type = BaseType;
-    using initializer_type = std::function<ptr_type_t<base_type> (std::string)>;
-    using ptr_type = ptr_type_t<base_type>;
-
-private:
-    struct type_info
-    {
-        initializer_type initializer;
-        std::string help;
-    };
-
-    using type_store = std::map<std::string, type_info>;
-
-public:
-    static void register_type(std::string type, initializer_type initializer)
-    {
-        types()[type].initializer = initializer;
-    }
-
-    template <typename ImplType>
-    static void register_type(std::string type)
-    {
-        register_type(type, [](std::string) {
-            ptr_type_t<base_type> p(new ImplType);
-            return p;
-        });
-    }
-
-    static ptr_type create(std::string const& type, std::string arg="")
-    {
-        typename type_store::const_iterator type_iter = types().find(type);
-        if(type_iter == types().end())
-        {
-            throw std::runtime_error(type + " not found");
-        }
-
-        return type_iter->second.initializer(arg);
-    }
-
-private:
-    static type_store & types()
-    {
-        static type_store TYPES;
-        return TYPES;
-    }
+template <typename BaseType> struct factory_traits {
+  using ptr_type = std::shared_ptr<BaseType>;
 };
 
+template <typename BaseType>
+using ptr_type_t = typename factory_traits<BaseType>::ptr_type;
 
-} // namespace yad
+template <typename BaseType> class factory {
+public:
+  using base_type = BaseType;
+  using initializer_type = std::function<ptr_type_t<base_type>(std::string)>;
+  using ptr_type = ptr_type_t<base_type>;
+
+private:
+  struct type_info {
+    initializer_type initializer;
+    std::string help;
+  };
+
+  using type_store = std::map<std::string, type_info>;
+
+public:
+  static void register_type(std::string type, initializer_type initializer) {
+    types()[type].initializer = initializer;
+  }
+
+  template <typename ImplType> static void register_type(std::string type) {
+    register_type(type, [](std::string) {
+      ptr_type_t<base_type> p(new ImplType);
+      return p;
+    });
+  }
+
+  static ptr_type create(std::string const &type, std::string arg = "") {
+    typename type_store::const_iterator type_iter = types().find(type);
+    if (type_iter == types().end()) {
+      throw std::runtime_error(type + " not found");
+    }
+
+    return type_iter->second.initializer(arg);
+  }
+
+private:
+  static type_store &types() {
+    static type_store TYPES;
+    return TYPES;
+  }
+};
+
+} // namespace yadi
 
 #endif // YADI_FACTORY_HPP__
--- test/yadi/factory_test.cpp
+++ test/yadi/factory_test.cpp
@@ -6,34 +6,45 @@
 
 namespace yadi {
 
-    struct my_type {
-    };
+struct my_type {};
 
-    struct my_type_1 : public my_type {
-    };
+struct my_type_1 : public my_type {};
 
-    struct my_raw_ptr;
+struct my_raw_ptr;
 
-    template <>
-    struct factory_traits<my_raw_ptr>
-    {
-        using ptr_type = std::unique_ptr<my_raw_ptr>;
-    };
+template <> struct factory_traits<my_raw_ptr> {
+  using ptr_type = my_raw_ptr *;
+};
 
-    struct my_raw_ptr : public factory<my_raw_ptr> { };
+struct my_raw_ptr : public factory<my_raw_ptr> {};
 
-    int main() {
-        {
-            factory<my_type>::register_type<my_type_1>("my_type_1");
-            ptr_type_t<my_type> p = factory<my_type>::create("my_type_1");
-        }
+struct my_unique_ptr;
 
-        {
-            my_raw_ptr::register_type<my_raw_ptr>("raw");
-            my_raw_ptr::ptr_type p = my_raw_ptr::create("raw");
-        }
+template <> struct factory_traits<my_unique_ptr> {
+  using ptr_type = std::unique_ptr<my_unique_ptr>;
+};
 
-        return 0;
-    }
+struct my_unique_ptr : public factory<my_unique_ptr> {};
 
 } // namespace yadi
+
+int main() {
+  using namespace yadi;
+
+  {
+    factory<my_type>::register_type<my_type_1>("my_type_1");
+    ptr_type_t<my_type> p = factory<my_type>::create("my_type_1");
+  }
+
+  {
+    my_raw_ptr::register_type<my_raw_ptr>("raw");
+    my_raw_ptr::ptr_type p = my_raw_ptr::create("raw");
+  }
+
+  {
+    my_unique_ptr::register_type<my_unique_ptr>("unique");
+    my_unique_ptr::ptr_type p = my_unique_ptr::create("unique");
+  }
+
+  return 0;
+}
