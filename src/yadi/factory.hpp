6dc91fe737e20920b4e116a9e11afc22cead09bb
Ed Clark <ebclark2@gmail.com>
2017-08-18 23:11:20 -0400

    Comments

--- src/yadi/factory.hpp
+++ src/yadi/factory.hpp
@@ -10,14 +10,25 @@
 
 namespace yadi {
 
+    /**
+     * @brief Factory traits that can be changed for base_t.
+     * @tparam base_t
+     */
 template <typename base_t>
 struct factory_traits {
-    using ptr_type = std::shared_ptr<base_t>;
+    using ptr_type = std::shared_ptr<base_t>; /// The type of pointer to return from create.
 };
 
+    /**
+     * @brief The type of pointer the base_t factory creates.
+     */
 template <typename base_t>
 using ptr_type_t = typename factory_traits<base_t>::ptr_type;
 
+    /**
+     * A YAML based factory.
+     * @tparam base_t
+     */
 template <typename base_t>
 class factory {
    public:
@@ -25,20 +36,31 @@ class factory {
     using initializer_type = std::function<ptr_type_t<base_type>(YAML::Node)>;
     using ptr_type = ptr_type_t<base_type>;
 
-   private:
     struct type_info {
         initializer_type initializer;
         std::string help;
     };
 
-   public:
     using type_store = std::map<std::string, type_info>;
 
    public:
+    /**
+     * @brief Registers initializer to type.  When create is called with type this initializer will be called.
+     * Overwrites initializer if already registered (will change).
+     * @param type
+     * @param initializer
+     */
     static void register_type(std::string type, initializer_type initializer) {
         mut_types()[type].initializer = initializer;
     }
 
+    /**
+     * @brief Calls the initializer associated with type passing the given YAML config.
+     * @param type
+     * @param config
+     * @return The result of the registered initializer
+     * @throws std::runtime_error if no initializer is registered for type
+     */
     static ptr_type create(std::string const& type, YAML::Node const& config = {}) {
         typename type_store::const_iterator type_iter = mut_types().find(type);
         if (type_iter == mut_types().end()) {
@@ -48,6 +70,13 @@ class factory {
         return type_iter->second.initializer(config);
     }
 
+    /**
+     * @brief Pulls type and config from YAML.  This function is especially usefil when loading nested types
+     * from YAML configuration.  If factory_config is a scalar string it will be used as type.  If factory_config
+     * is a map then "type" and "config" keys will be pulled from it and used as such.
+     * @param factory_config
+     * @return
+     */
     static ptr_type create(YAML::Node const& factory_config) {
         if (!factory_config.IsDefined()) {
             throw std::runtime_error("Factory config not defined");
@@ -79,6 +108,10 @@ class factory {
         throw std::runtime_error("Factory config not valid, YAML must be scalar string or map");
     }
 
+    /**
+     * @brief A copy of the stored types and their registered initializers and help.
+     * @return
+     */
     static type_store types() { return mut_types(); }
 
    private:
@@ -91,6 +124,14 @@ class factory {
 template <typename base_t>
 using initializer_type_t = typename factory<base_t>::initializer_type;
 
+    /**
+     * @brief Constructs impl_t via a constructor that accepts YAML and returns as pointer to base_t,
+     * ptr_type_t<base_t>.
+     * @tparam base_t Use to determine pointer type.
+     * @tparam impl_t Type to construct
+     * @param config Argument to constructor.
+     * @return The constructed type as ptr_type_t<base_t>
+     */
 template <typename base_t, typename impl_t>
 ptr_type_t<base_t> yaml_init(YAML::Node const& config) {
     ptr_type_t<base_t> ret(new impl_t(config));
@@ -98,18 +139,42 @@ ptr_type_t<base_t> yaml_init(YAML::Node const& config) {
 };
 
 // TODO complete function
+    // TODO change argument names
+    /**
+     * If both types are maps then they are merged.  If right is not defined then left is used.  Otherwise, error.
+     * @param left
+     * @return
+     */
 inline YAML::Node merge_yaml(YAML::Node const& left, YAML::Node const& /* right */) { return left; }
 
+    /**
+     * @brief Equivalent to factory<baes_t>::register_type(type, initializer)
+     * @tparam base_t
+     * @param type
+     * @param initializer
+     */
 template <typename base_t>
 void register_type(std::string type, initializer_type_t<base_t> initializer) {
     factory<base_t>::register_type(type, initializer);
 };
 
+    /**
+     * @brief Registers type using yaml_init function as initializer.
+     * @tparam base_t
+     * @tparam impl_t
+     * @param type
+     */
 template <typename base_t, typename impl_t>
 void register_type(std::string type) {
     register_type<base_t>(type, &yaml_init<base_t, impl_t>);
 };
 
+    /**
+     * @brief Registers type to initializer that will construct impl_t using default constructor.
+     * @tparam base_t
+     * @tparam impl_t
+     * @param type
+     */
 template <typename base_t, typename impl_t>
 void register_type_no_arg(std::string type) {
     register_type<base_t>(type, [](YAML::Node) {
@@ -118,6 +183,14 @@ void register_type_no_arg(std::string type) {
     });
 }
 
+    /**
+     * @brief Registers alias to type and config pair.  When create is called for alias the passed in and registered
+     * configs are merged and the initializer registered to type is called with the result.
+     * @tparam base_t
+     * @param alias
+     * @param type
+     * @param config
+     */
 template <typename base_t>
 void register_alias(std::string alias, std::string type, YAML::Node config) {
     register_type<base_t>(alias, [type, config](YAML::Node const& passedConfig) {
@@ -126,6 +199,16 @@ void register_alias(std::string alias, std::string type, YAML::Node config) {
     });
 }
 
+    /**
+     * @brief Loads aliases from a YAML file.  The file should be a map of the format...
+     * alias:
+     *   type: actualType
+     *   config: ...
+     *
+     * For each entry register_alias() is called.
+     * @tparam base_t
+     * @param aliases
+     */
 template <typename base_t>
 void register_aliases(YAML::Node aliases) {
     // TODO error handling
