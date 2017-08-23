704f1058363873c384ece1cef185c9ea5b343779
Ed Clark <ebclark2@gmail.com>
2017-08-22 22:25:09 -0400

    Convertion to factory initializer

--- test/yadi/function_test.cpp
+++ test/yadi/function_test.cpp
@@ -14,55 +14,6 @@
  * implementation of sorts, so long as I don't forget what I'm doing.
  */
 
-template <size_t N, typename Tuple_Type>
-struct tuple_trunc {};
-
-template <size_t N, typename Head, typename... Tail>
-struct tuple_trunc<N, std::tuple<Head, Tail...>> {
-    typedef typename tuple_trunc<N - 1, std::tuple<Tail...>>::type type;
-};
-
-template <typename Head, typename... Tail>
-struct tuple_trunc<0, std::tuple<Head, Tail...>> {
-    typedef std::tuple<Head, Tail...> type;
-};
-
-template <size_t N, size_t I>
-struct assign {
-    template <class ResultTuple, class SrcTuple>
-    static void x(ResultTuple& t, const SrcTuple& tup) {
-        std::get<I - N>(t) = std::get<I>(tup);
-        assign<N, I - 1>::x(t, tup);  // this offsets the assignment index by N
-    }
-};
-
-template <size_t N>
-struct assign<N, 1> {
-    template <class ResultTuple, class SrcTuple>
-    static void x(ResultTuple& t, const SrcTuple& tup) {
-        std::get<0>(t) = std::get<1>(tup);
-    }
-};
-
-template <size_t TruncSize, class Tup>
-struct th2;
-
-template <size_t TruncSize, class Head, class... Tail>
-struct th2<TruncSize, std::tuple<Head, Tail...>> {
-    typedef typename tuple_trunc<TruncSize, std::tuple<Head, Tail...>>::type type;
-
-    static type tail(const std::tuple<Head, Tail...>& tup) {
-        type t;
-        assign<TruncSize, std::tuple_size<type>::value>::x(t, tup);
-        return t;
-    }
-};
-
-template <size_t TruncSize, class Tup>
-typename th2<TruncSize, Tup>::type tail(const Tup& tup) {
-    return th2<TruncSize, Tup>::tail(tup);
-}
-
 template <typename tuple_t, size_t index = std::tuple_size<tuple_t>::value - 1>
 struct print_thing {
     static void print(YAML::Node const& yaml) {
@@ -107,10 +58,10 @@ void printer(T const&, YAML::Node const& yaml) {
 }
 
 template <typename T>
-struct function_args_to_tuple_type;
+struct function_args_to_tuple;
 
 template <typename R, typename... Args>
-struct function_args_to_tuple_type<R (*)(Args...)> {
+struct function_args_to_tuple<R (*)(Args...)> {
     using result_type = R;
     using params_type = std::tuple<Args...>;
 
@@ -128,13 +79,13 @@ struct function_args_to_tuple_type<R (*)(Args...)> {
         // Use std::apply in c++17
         params_type params;
         print_thing<params_type>::to_tuple(params, yaml);
-        function_args_to_tuple_type f = {func, params};
+        function_args_to_tuple f = {func, params};
         return f.delayed_dispatch();
     }
 };
 
 template <typename R, typename... Args>
-struct function_args_to_tuple_type<std::function<R(Args...)>> {
+struct function_args_to_tuple<std::function<R(Args...)>> {
     using result_type = R;
     using params_type = std::tuple<Args...>;
 
@@ -152,21 +103,26 @@ struct function_args_to_tuple_type<std::function<R(Args...)>> {
         // Use std::apply in c++17
         params_type params;
         print_thing<params_type>::to_tuple(params, yaml);
-        function_args_to_tuple_type f = {func, params};
+        function_args_to_tuple f = {func, params};
         return f.delayed_dispatch();
     }
 };
 
 template <typename T>
-using function_args_to_tuple_type_params_type = typename function_args_to_tuple_type<T>::params_type;
+using function_args_to_tuple_params_type = typename function_args_to_tuple<T>::params_type;
 
 template <typename T>
-using function_args_to_tuple_type_result_type = typename function_args_to_tuple_type<T>::result_type;
+using function_args_to_tuple_result_type = typename function_args_to_tuple<T>::result_type;
 
 template <typename F>
-function_args_to_tuple_type_result_type<F> call_from_yaml(F const& func, YAML::Node const& yaml) {
-    return function_args_to_tuple_type<F>::call(func, yaml);
+function_args_to_tuple_result_type<F> call_from_yaml(F const& func, YAML::Node const& yaml) {
+    return function_args_to_tuple<F>::call(func, yaml);
 }
+
+template <typename base_t, typename F>
+::yadi::initializer_type_t<base_t> make_initializer(F func) {
+    return [func](YAML::Node const& yaml) { return call_from_yaml(func, yaml); };
+};
 
 double sum(double a1, double a2, int a3, double a4) { return a1 + a2 + a3 + a4; }
 
@@ -183,9 +139,8 @@ YADI_TEST(function_test) {
 - 4.5
 )raw";
     using ctr_sig = std::function<double(double, double, int, double)>;
-    //    std::tuple<double, double, int, double> test; //(1, 2, 3, 4);
-    function_args_to_tuple_type_params_type<ctr_sig> test;
-    // tuple_trunc<2, std::tuple<double, double, int, double>>::type c = tail<2>(test);
+
+    function_args_to_tuple_params_type<ctr_sig> test;
     printer(test, YAML::Load(YAML));
     print_thing<decltype(test)>::to_tuple(test, YAML::Load(YAML));
     YADI_ASSERT_EQ(std::get<0>(test), 1.5);
@@ -201,6 +156,12 @@ YADI_TEST(function_test) {
     {
         double result = call_from_yaml(&Foo::sum, YAML::Load(YAML));
         YADI_ASSERT_EQ(11.5, result);
+    }
+
+    {
+        // create initializer type the factory will use...
+        auto initializer = make_initializer<double>(&sum);
+        YADI_ASSERT_EQ(11.5, initializer(YAML::Load(YAML)));
     }
 
     return true;
