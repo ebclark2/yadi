bc215c4aaa72181ee9f97207a98e3179c608dc4d
Ed Clark <ebclark2@gmail.com>
2017-08-29 19:52:00 -0400

    Standardized template casing

--- src/yadi/yadi.cpp
+++ src/yadi/yadi.cpp
@@ -30,4 +30,19 @@ std::string demangle(const char* name) { return name; }
 
 #endif
 
+yadi_help_fetcher::yadi_help_fetcher() {}
+yadi_help_fetcher::yadi_help_fetcher(yadi_help_fetcher const& other) : impl(other.impl->clone().release()) {}
+
+yadi_help_fetcher& yadi_help_fetcher::operator=(yadi_help_fetcher const& other) {
+    this->impl = other.impl->clone();
+    return *this;
+}
+
+yadi_help::help_store const& yadi_help::helps() { return mut_helps(); }
+
+yadi_help::help_store& yadi_help::mut_helps() {
+    static help_store HELPS;
+    return HELPS;
+}
+
 }  // namespace yadi
