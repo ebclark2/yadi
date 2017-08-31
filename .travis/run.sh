1c4eb3816864f955c128fb63415202e156424d4a
Ed Clark <ed.clark@gliacelltechnologies.com>
2017-08-31 08:34:43 -0400

    Added remote with new dependency

--- .travis/run.sh
+++ .travis/run.sh
@@ -9,5 +9,5 @@ fi
 pyenv activate conan
 
 mkdir build
-cd build && conan install .. --build && cmake .. && make && ctest
+cd build && conan remote add edsbin https://api.bintray.com/conan/edsidea/edsidea && conan install .. --build && cmake .. && make && ctest
 #python build.py
