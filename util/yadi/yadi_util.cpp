266e2a90fe2b68234c6f1d579ab865fabb533d27
Ed Clark <ebclark2@gmail.com>
2017-08-30 20:42:36 -0400

    util refactor

--- util/yadi/yadi_util.cpp
+++ util/yadi/yadi_util.cpp
@@ -2,6 +2,20 @@
 // Created by Ed Clark on 8/26/17.
 //
 
-#include "yadi_util.hpp"
+#include <yadi/yadi.hpp>
 
-namespace yadi {}  // namespace yadi
+#include <iostream>
+
+int main() {
+    using namespace yadi;
+    yadi_help::help_store helps = yadi_help::helps();
+    for (auto const& entry : helps) {
+        std::cout << entry.first << '\n';
+        std::vector<std::string> types = entry.second.getTypes();
+        for (std::string const& type : types) {
+            std::cout << "\t\"" << type << "\" -> " << entry.second.getHelp(type) << '\n';
+        }
+    }
+
+    return 0;
+}
