b2ebbec4232c14eb03c830ee52cfa0e9cbb7ff51
Ed Clark <ebclark2@gmail.com>
2017-09-03 19:45:14 -0400

    Travis update

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,18 +8,16 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew outdated pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv
     brew install cmake || true
-else
-    curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-fi
 
-if which pyenv > /dev/null; then
-    eval "$(pyenv init -)"
-fi
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
 
-pyenv install 2.7.10
-pyenv virtualenv 2.7.10 conan
-pyenv rehash
-pyenv activate conan
+    pyenv install 2.7.10
+    pyenv virtualenv 2.7.10 conan
+    pyenv rehash
+    pyenv activate conan
+fi
 
 pip install conan --upgrade
 pip install conan_package_tools
--- .travis/run.sh
+++ .travis/run.sh
@@ -3,11 +3,12 @@
 set -e
 set -x
 
-if which pyenv > /dev/null; then
-    eval "$(pyenv init -)"
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
 fi
-pyenv activate conan
 
-mkdir build
-cd build && conan remote add edsbin https://api.bintray.com/conan/edsidea/edsidea && conan install .. --build && cmake .. && make && ctest
-#python build.py
+conan remote add edsbin https://api.bintray.com/conan/edsidea/edsidea
+python build.py
