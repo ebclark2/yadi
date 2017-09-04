10f976e255d52fcf5793e24e617ac94dd38ddaf6
Ed Clark <ebclark2@gmail.com>
2017-09-03 23:14:01 -0400

    prebuilt did not work

--- build.py
+++ build.py
@@ -2,7 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=['x86_64'])
-#    builder = ConanMultiPackager(args="--build", archs=['x86_64'])
+    builder = ConanMultiPackager(args="--build", archs=['x86_64'])
     builder.add_common_builds()
     builder.run()
