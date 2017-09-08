acfb45e5ac19ecbcbbe76e4e74f3f6eda150b6ef
Ed Clark <ebclark2@gmail.com>
2017-09-08 06:18:53 -0400

    Removed silly comments in demangle

--- src/yadi/details/demangle.cpp
+++ src/yadi/details/demangle.cpp
@@ -17,9 +17,7 @@ namespace yadi {
 #ifdef YADI_HAS_DEMANGLE
 
 std::string demangle(const char* name) {
-    int status = -4;  // some arbitrary value to eliminate the compiler warning
-
-    // enable c++11 by passing the flag -std=c++11 to g++
+    int status = -1;
     std::unique_ptr<char, void (*)(void*)> res{abi::__cxa_demangle(name, NULL, NULL, &status), std::free};
 
     return (status == 0) ? res.get() : name;
