c7cdf347d33ee5a816e9d34aed75dbb44ef3d022
Ed Clark <ebclark2@gmail.com>
2017-09-14 23:37:09 -0400

    documentation link on readme

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['yadi_20_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2febclark2_2fyadi_2esvg_3fbranch_3dmaster_29',['YADI ![Build Status](https://travis-ci.org/ebclark2/yadi.svg?branch=master)',['../index.html',1,'']]],
+  ['yadi_20_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2febclark2_2fyadi_2esvg_3fbranch_3dmaster_29_20_21_5bdocumentation_5d_28https_3a_2f_2febclark2_2egithub_2eio_2fyadi_2f_29',['YADI ![Build Status](https://travis-ci.org/ebclark2/yadi.svg?branch=master) ![Documentation](https://ebclark2.github.io/yadi/)',['../index.html',1,'']]],
   ['yadi',['yadi',['../namespaceyadi.html',1,'']]],
   ['yadi_5fhelp',['yadi_help',['../structyadi_1_1yadi__help.html',1,'yadi']]],
   ['yadi_5fhelp_5ffetcher',['yadi_help_fetcher',['../structyadi_1_1details_1_1yadi__help__fetcher.html',1,'yadi::details']]],
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['yadi_20_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2febclark2_2fyadi_2esvg_3fbranch_3dmaster_29',['YADI ![Build Status](https://travis-ci.org/ebclark2/yadi.svg?branch=master)',['../index.html',1,'']]]
+  ['yadi_20_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2febclark2_2fyadi_2esvg_3fbranch_3dmaster_29_20_21_5bdocumentation_5d_28https_3a_2f_2febclark2_2egithub_2eio_2fyadi_2f_29',['YADI ![Build Status](https://travis-ci.org/ebclark2/yadi.svg?branch=master) ![Documentation](https://ebclark2.github.io/yadi/)',['../index.html',1,'']]]
 ];
