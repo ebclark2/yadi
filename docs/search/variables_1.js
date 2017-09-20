ad793c712419f9657a488d19c6fe59eb14af68cb
Ed Clark <ebclark2@gmail.com>
2017-09-19 22:18:34 -0400

    yadi_info class comment

---
+++ docs/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['help',['help',['../structyadi_1_1factory_1_1yadi__info.html#a5b9a7705a796247d0049755236516015',1,'yadi::factory::yadi_info']]]
+];
---
+++ docs/search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['initializer',['initializer',['../structyadi_1_1factory_1_1yadi__info.html#a63c0e3eb9fd1e8051f5aacee3439c313',1,'yadi::factory::yadi_info']]]
+];
